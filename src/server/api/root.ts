c581079502fabef953a15fa3e1461ba014eff648
peterdinis <petter1@pobox.sk>
2025-04-27 16:06:17 +0200

    feat: Prepare new routes

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,19 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { adminRouter } from "./routers/admin";
+import { cartRouter } from "./routers/cart";
+import { categoryRouter } from "./routers/category";
+import { productRouter } from "./routers/product";
+import { userRouter } from "./routers/user";
+import { paymentRouter } from "./routers/payment";
 
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+    admin: adminRouter,
+    cart: cartRouter,
+    category: categoryRouter,
+    product: productRouter,
+    user: userRouter,
+    payment: paymentRouter
+});
 
 export type AppRouter = typeof appRouter;
 
