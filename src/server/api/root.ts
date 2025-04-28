fb7d1db70b1bdbb771329ff0d02b0b2fe2f42aed
peterdinis <petter1@pobox.sk>
2025-04-28 18:27:54 +0200

    feat: New column isAviable

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,7 +1,6 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { adminRouter } from "./routers/admin";
 import { cartRouter } from "./routers/cart";
-import { categoryRouter } from "./routers/category";
 import { paymentRouter } from "./routers/payment";
 import { productRouter } from "./routers/product";
 import { userRouter } from "./routers/user";
@@ -9,7 +8,6 @@ import { userRouter } from "./routers/user";
 export const appRouter = createTRPCRouter({
 	admin: adminRouter,
 	cart: cartRouter,
-	category: categoryRouter,
 	product: productRouter,
 	user: userRouter,
 	payment: paymentRouter,
