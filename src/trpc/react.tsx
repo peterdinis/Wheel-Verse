55a257e3413593c6ce4114cd69ce29b2fdce92ad
peterdinis <petter1@pobox.sk>
2025-05-08 18:13:03 +0200

    infra: New suspense component

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -4,11 +4,12 @@ import { type QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchStreamLink, loggerLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import SuperJSON from "superjson";
 
 import type { AppRouter } from "~/server/api/root";
 import { createQueryClient } from "./query-client";
+import { Loader2 } from "lucide-react";
 
 let clientQueryClientSingleton: QueryClient | undefined = undefined;
 const getQueryClient = () => {
@@ -64,9 +65,11 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<api.Provider client={trpcClient} queryClient={queryClient}>
-				{props.children}
-			</api.Provider>
+			<Suspense fallback={<Loader2 className="animate-spin w-8 h-8" />}>
+				<api.Provider client={trpcClient} queryClient={queryClient}>
+					{props.children}
+				</api.Provider>
+			</Suspense>
 		</QueryClientProvider>
 	);
 }
